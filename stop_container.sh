9a96b079c8dccdaa875a5413dc0a9e2a97b5a5fc
Rituraj-Debnath <riturajdebnathfit@gmail.com>
2025-08-06 12:32:08 +0530

    Updated stop_container.sh

--- stop_container.sh
+++ stop_container.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-echo "is it working?"
-
+# Stop the running container (if exists)
+CONTAINER_ID=$(docker ps -q --filter ancestor=rex03/simple-python-app)
+if [ ! -z "$CONTAINER_ID" ]; then
+  echo "Stopping container $CONTAINER_ID..."
+  docker stop "$CONTAINER_ID"
+else
+  echo "No running container found."
+fi
